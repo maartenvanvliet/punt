e5c4c113cc6f7cb242b6554ea218aa1424481c6c
Maarten van Vliet <maartenvanvliet@users.noreply.github.com>
2023-01-19 21:48:16 +0100

    Version bump

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Punt.MixProject do
   def project do
     [
       app: :punt,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
